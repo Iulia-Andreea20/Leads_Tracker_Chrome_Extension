434c2dcef72888b52ca5d8dabf2aace0334d626e
Grigore Iulia-Andreea <40318390+Iulia-Andreea20@users.noreply.github.com>
2022-05-07 02:15:03 +0300

    add save current link from the tab button

--- functionality.js
+++ functionality.js
@@ -11,7 +11,11 @@ const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 let errMsg = document.getElementById("err-msg");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+// console.log(leadsFromLocalStorage);
+
 
 //! Set key with the value value, get it, to see that
 //! it successfully set storage the data and then,
@@ -24,19 +28,24 @@ let errMsg = document.getElementById("err-msg");
 // localStorage.clear()
 // localStorage.clear()
 
-deleteBtn.addEventListener("dblclick", function () {
-    localStorage.clear();
-    myLeads = [];
-    // ulEl.innerHTML = "" or readderLeads()
-    render()
-})
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-console.log(leadsFromLocalStorage);
-
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
     render(myLeads);
 }
+
+tabBtn.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        render(myLeads)
+    })
+
+
+})
+
+// const tabs = [
+//     { url: "google.com" }
+// ]
 
 function render(leads) {
     let listItems = ""
@@ -67,6 +76,13 @@ function render(leads) {
     ulEl.innerHTML = listItems;
 }
 
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear();
+    myLeads = [];
+    // ulEl.innerHTML = "" or readderLeads()
+    render(myLeads)
+})
+
 //! Event Listener
 inputBtn.addEventListener("click", function () {
     //! Get the value of the input element
@@ -75,8 +91,8 @@ inputBtn.addEventListener("click", function () {
     inputEl.value = "";
 
     localStorage.setItem("myLeads", JSON.stringify(myLeads));
-    render()
-    console.log(localStorage.getItem("myLeads"));
+    render(myLeads)
+    // console.log(localStorage.getItem("myLeads"));
 
 })
 
